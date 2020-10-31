996d61ff1337ee86b7c6b8c91d33b1ada35fc0ca
nosequiensoy007 <72319747+nosequiensoy007@users.noreply.github.com>
2020-10-31 16:38:13 -0600

    Add files via upload

--- Plot_4.R
+++ Plot_4.R
@@ -16,8 +16,12 @@ info[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
 info <- info[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
 png("plot4.png", width=480, height=480)
-
 par(mfrow=c(2,2))
-plot(info[, dateTime], info[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
-
+with(info, plot(dateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
+with(info, plot(dateTime, Voltage, type = "l", xlab="datetime", ylab="Voltage"))
+with(info, plot(dateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+lines(info$dateTime, info$Sub_metering_2,type="l", col= "red")
+lines(info$dateTime, info$Sub_metering_3,type="l", col= "blue")
+legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
+with(info, plot(dateTime, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
 dev.off()
